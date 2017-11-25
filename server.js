0e8fa8d80eed01d3ef171f372b787abf387d9733
tlindener <tobias.lindener@outlook.com>
2017-11-25 16:43:47 +0200

    Fix loop

--- server.js
+++ server.js
@@ -4,42 +4,58 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express'); // call express
+var app = express(); // define our app using express
 var bodyParser = require('body-parser');
 // Load Chance
 var Chance = require('chance');
 
 // Instantiate Chance so it can be used
 var chance = new Chance();
+// var NodeGeocoder = require('node-geocoder');
+// var options = {
+//   provider: 'google',
+//
+//   // Optional depending on the providers
+//   httpAdapter: 'https', // Default
+//   apiKey: 'YOUR_API_KEY', // for Mapquest, OpenCage, Google Premier
+//   formatter: null         // 'gpx', 'string', ...
+// };
+//
+// var geocoder = NodeGeocoder(options);
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.VCAP_APP_PORT || 8080; // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 var recommendations = require('./data/recommendations');
 
 router.get('/recommendations/fb', function(req, res) {
-    var recommendations_new = []
-    for(var i; i<= 5;i++){
-      recommendations.forEach(function item){
-        item['month'] = chance.integer({min: 1, max: 12});
-        recommendations_new.append(item)
-      }
-    }
-    res.json(recommendations_new);
+  console.log("/recommendations/fb");
+  var recommendations_new = []
+
+  recommendations.forEach(function(item) {
+    item['month'] = chance.integer({
+      min: 1,
+      max: 12
+    });
+    recommendations_new.push(item)
+  });
+  res.json(recommendations_new);
 });
 
 var airports = require('./data/airports');
 router.get('/airports', function(req, res) {
-    res.json(airports);
+  res.json(airports);
 });
 // more routes for our API will happen here
 
